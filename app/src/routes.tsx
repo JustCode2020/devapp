26a12e61a7d9883eba9a574b90d9e65f6823e7b3
ewerter <ewertontertuliano@gmail.com>
2020-08-14 18:49:49 -0400

    test route to  display map component

--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route, BrowserRouter} from 'react-router-dom';
 import Home from './pages/Home';
 import Point from './pages/Point';
+import MapTest from './pages/Map/MapHook'
 
 const Routes = () => {
     return(
         <BrowserRouter>
             <Route component= {Home} path="/" exact/>
             <Route component= {Point} path="/point"/>
+            <Route component={MapTest} path='/test'/>
         </BrowserRouter>
     );
 }
